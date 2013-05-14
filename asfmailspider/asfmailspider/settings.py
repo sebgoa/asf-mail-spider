69c32f86adfc6dae00c44ccc2205d699b6aff7b3
Sebastien Goasguen <runseb@gmail.com>
2013-05-14 09:27:03 -0400

    Adding ASF License headers

--- asfmailspider/asfmailspider/settings.py
+++ asfmailspider/asfmailspider/settings.py
@@ -1,10 +1,19 @@
-# Scrapy settings for asf project
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
 #
-# For simplicity, this file contains only the most important settings by
-# default. All the other settings are documented here:
+#   http://www.apache.org/licenses/LICENSE-2.0
 #
-#     http://doc.scrapy.org/topics/settings.html
-#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
 
 BOT_NAME = 'asf'
 
